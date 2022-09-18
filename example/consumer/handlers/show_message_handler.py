7bb21d37d91db543d9639439354f4bbacae3d677
zuhairmhtb <zuhairmhtb@gmail.com>
2022-09-18 23:13:10 +0600

    Updated poetry version

--- example/consumer/handlers/show_message_handler.py
+++
@@ -1,9 +0,0 @@
-from PyMessagingFramework.src.PyMessagingFramework.framework import BaseCommandHandler
-from PyMessagingFramework.example.consumer.commands.show_message_command import ShowMessageCommand
-
-class ShowMessageHandler(BaseCommandHandler):
-    
-    def handle(self, command:ShowMessageCommand):
-        print(f"Received message: {command.message}")
-        print(f"Received number: {command.number}")
-        print(f"Received list: {command.array}")
